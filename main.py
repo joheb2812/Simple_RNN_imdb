54af8162528caaa9ccc2eda91f01f64f0d8aae3e
Joheb Dani <johebdani@gmail.com>
2025-02-06 11:57:47 +0530

    Changes in main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Step 1: Import Libraries and Load the Model
 import numpy as np
+import keras
 import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
 
+print(f"Keras version: {keras.__version__}")
+print(f"TensorFlow version: {tf.__version__}")
 # Load the IMDB dataset word index
 word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
@@ -25,11 +28,11 @@ def preprocess_text(text):
     return padded_review
 
 #Step3: Creating prediction function
-def predict_sentiment(review):
-    preprocessed_input= preprocess_text(review)
-    prediction = model.predict(preprocessed_input)
-    sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
-    return sentiment, prediction[0][0]
+# def predict_sentiment(review):
+#     preprocessed_input= preprocess_text(review)
+#     prediction = model.predict(preprocessed_input)
+#     sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+#     return sentiment, prediction[0][0]
 
 # Design streamlit app 
 import streamlit as st
